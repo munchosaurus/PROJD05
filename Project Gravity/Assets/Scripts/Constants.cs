bf31b2951dbd6e4c85ca3f493f02f79a358c4ae8
munchosaurus <martinkings1@live.se>
2022-11-01 12:43:35 +0100

    Changed constants

--- Project Gravity/Assets/Scripts/Constants.cs
+++ Project Gravity/Assets/Scripts/Constants.cs
@@ -18,13 +18,13 @@ public static class Constants
     
     
     // Add finished player input settings here:
-    public const float PLAYER_JUMP_FORCE = 3;
-    public const float PLAYER_JUMP_COOLDOWN = 0.5f;
-    public const float PLAYER_MAX_WALKING_VELOCITY = 4f;
-    public const float PLAYER_MOVEMENT_ACCELERATION = 25f;
+    public const float PLAYER_JUMP_FORCE = 10f;
+    public const float PLAYER_JUMP_COOLDOWN = 1f;
+    public const float PLAYER_MAX_WALKING_VELOCITY = 8f;
+    public const float PLAYER_MOVEMENT_ACCELERATION = 40f;
     public const float PLAYER_MOVEMENT_DECELERATION = 15f;
     public const float PLAYER_JUMP_FORCE_MULTIPLIER = 100f;
-    public const float PLAYER_AIR_MOVEMENT_MULTIPLIER = 0.2f;
+    public const float PLAYER_AIR_MOVEMENT_MULTIPLIER = 0.1f;
     public const float PLAYER_AIMING_POINT_POSITIONING_MULTIPLIER = 1f;
     public const float PLAYER_Z_VALUE = 1f;
     public const float PLAYER_AIR_SPEED_DAMPER = 0.1f;
