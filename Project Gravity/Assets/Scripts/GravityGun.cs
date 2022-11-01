f89899bcdfbb83843c545df607bf2d22b93f05c5
munchosaurus <martinkings1@live.se>
2022-11-01 18:07:59 +0100

    Aiming direction arrow added

--- Project Gravity/Assets/Scripts/GravityGun.cs
+++ Project Gravity/Assets/Scripts/GravityGun.cs
@@ -10,6 +10,7 @@ public class GravityGun : MonoBehaviour
     [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private LayerMask gravityMask;
+    [SerializeField] private GameObject aimDirector;
     private Vector3 _currentDirection;
     private GameObject crosshair;
     private MeshRenderer crosshairMesh;
@@ -17,9 +18,7 @@ public class GravityGun : MonoBehaviour
 
     private void Awake()
     {
-        //crosshair = GameObject.FindGameObjectWithTag("Crosshair");
-        //crosshairMesh = crosshair.GetComponent<MeshRenderer>();
-        //crosshairMesh.enabled = false;
+        aimDirector = GameObject.FindGameObjectWithTag("AimingDirector");
         _lineRenderer = GameObject.FindWithTag("LineRenderer").GetComponent<LineRenderer>();
     }
 
@@ -39,17 +38,12 @@ public class GravityGun : MonoBehaviour
             {
                 _lineRenderer.gameObject.SetActive(true);
             }
-
-            // if (crosshairMesh.enabled == false)
-            // {
-            //     crosshairMesh.enabled = true;
-            // }
+            
             SetCrosshair();
         }
         else
         {
             _lineRenderer.gameObject.SetActive(false);
-            //crosshairMesh.enabled = false;
         }
     }
 
@@ -66,21 +60,75 @@ public class GravityGun : MonoBehaviour
             if ((float)Math.Round(Vector3.Distance(transform.position, gravityHit.point), 2) >
                 (float)Math.Round(Vector3.Distance(transform.position, groundHit.point), 2))
             {
+                DisableAimDirector();
                 _lineRenderer.material = lineMaterials[0];
                 linePosition = groundHit.point * Constants.PLAYER_AIMING_POINT_POSITIONING_MULTIPLIER;
             }
             else
             {
                 _lineRenderer.material = lineMaterials[1];
+                if (gravityHit.normal != -GravityController.GetCurrentFacing())
+                {
+                    EnableAimDirector(gravityHit);
+                }
                 linePosition = gravityHit.point * Constants.PLAYER_AIMING_POINT_POSITIONING_MULTIPLIER;
             }
         }
         else
         {
+            DisableAimDirector();
             _lineRenderer.material = lineMaterials[0];
             linePosition = groundHit.point * Constants.PLAYER_AIMING_POINT_POSITIONING_MULTIPLIER;
         }
         _lineRenderer.SetPosition(1, linePosition);
+    }
+
+    private void EnableAimDirector(RaycastHit hit)
+    {
+        if (!aimDirector.GetComponent<SpriteRenderer>().enabled)
+        {
+            aimDirector.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        
+        aimDirector.transform.position = new Vector3(hit.point.x,
+            hit.point.y - 1, 2);
+
+        if (hit.normal.x != 0)
+        {
+            if (hit.point.x > transform.position.x)
+            {
+                aimDirector.transform.position = new Vector3(hit.point.x - 0.5f,
+                    hit.point.y, 2);
+            }
+            else
+            {
+                aimDirector.transform.position = new Vector3(hit.point.x + 0.5f,
+                    hit.point.y, 2);
+            }
+        } else if (hit.normal.y != 0)
+        {
+            if (hit.point.y > transform.position.y)
+            {
+                aimDirector.transform.position = new Vector3(hit.point.x,
+                    hit.point.y - 0.5f, 2);
+            }
+            else
+            {
+                aimDirector.transform.position = new Vector3(hit.point.x,
+                    hit.point.y + 0.5f, 2);
+            }
+        }
+
+        aimDirector.transform.rotation = Quaternion.LookRotation(transform.forward, hit.normal);
+
+    }
+
+    private void DisableAimDirector()
+    {
+        if (aimDirector.GetComponent<SpriteRenderer>().enabled)
+        {
+            aimDirector.GetComponent<SpriteRenderer>().enabled = false;
+        }
     }
 
     private void TriggerGravityGunEvent(RaycastHit hit)
@@ -98,7 +146,7 @@ public class GravityGun : MonoBehaviour
     {
         RaycastHit groundHit;
         RaycastHit gravityHit;
-
+        DisableAimDirector();
         Physics.Raycast(transform.position, _currentDirection, out groundHit, Mathf.Infinity, groundMask);
         if (Physics.Raycast(transform.position, _currentDirection, out gravityHit, Mathf.Infinity,
                 gravityMask,
@@ -129,7 +177,6 @@ public class GravityGun : MonoBehaviour
         if (val.canceled)
         {
             _lineRenderer.gameObject.SetActive(false);
-            //crosshairMesh.enabled = false;
             ShootGravityGun();
         }
     }
