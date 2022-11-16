7351277fe1dcce4deb7f0fef797e5041486ee1f1
GUST <gustavkjonsson01@gmail.com>
2022-11-16 16:03:28 +0100

    New Skybox & Skybox Rotation Script

---
+++ Project Gravity/Assets/Scripts/RotateSky.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour {
+
+    public float RotateSpeed = 1.2f;
+    
+    void Update() {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+
+    }
+}
+
+//Tutorial for Reference:
+//https://youtu.be/cqGq__JjhMM
