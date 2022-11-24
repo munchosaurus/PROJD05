1b46f75d2e1fb4e536ca271b26ba5c7f6c52b01a
munchosaurus <martinkings1@live.se>
2022-11-24 11:18:47 +0100

    Ember rotation based on gravity

--- Project Gravity/Assets/RotateEmbers.cs
+++ Project Gravity/Assets/RotateEmbers.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RotateEmbers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static Guid _gravityGunEventGuid;
+    private static Vector3 _facing;
+    private static Quaternion _rotationQuaternion;
+    private GameObject[] _embers;
+    private static int _gravityLayer;
+
+    private void Start()
     {
-        
+        EventSystem.Current.RegisterListener<GravityGunEvent>(OnGravityGunHit, ref _gravityGunEventGuid);
+        _facing = GravityController.GetCurrentFacing();
+        _embers = GameObject.FindGameObjectsWithTag("EmberRotation");
+        _gravityLayer = LayerMask.NameToLayer("GravityChange");
+    }
+    
+    void FixedUpdate()
+    {
+        if (_embers.Length > 0)
+        {
+            foreach (var ember in _embers)
+            {
+                ember.transform.rotation = Quaternion.Slerp(ember.transform.rotation, _rotationQuaternion, Time.fixedDeltaTime * Constants.GRAVITY_ARROW_ROTATION_SPEED);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGravityGunHit(GravityGunEvent gravityGunEvent)
     {
-        
+        if (gravityGunEvent.TargetGameObject.layer == _gravityLayer)
+        {
+            _facing = GravityController.GetCurrentFacing();
+            _rotationQuaternion = FindRotation();
+        }
     }
+
+    private Quaternion FindRotation()
+    {
+        return Quaternion.LookRotation(transform.forward, _facing);
+    }
+    
+
+
 }
