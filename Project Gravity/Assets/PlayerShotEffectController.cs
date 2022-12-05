6f27f8552e7a5ba8d8f4fea9a4bd9c5d5b763875
munchosaurus <martinkings1@live.se>
2022-12-05 16:56:24 +0100

    Impact colors for effect added to prefab

--- Project Gravity/Assets/PlayerShotEffectController.cs
+++ Project Gravity/Assets/PlayerShotEffectController.cs
@@ -8,10 +8,18 @@ using UnityEngine.VFX;
 public class PlayerShotEffectController : MonoBehaviour
 {
     [SerializeField] private GameObject laserShotPrefab;
+    [SerializeField] private Color[] colors;
+    [SerializeField] private float impactSize;
     private static Guid _playerShootsGuid;
+    private static int gravityLayer;
+    private static int groundLayer;
+    private static int magnetLayer;
 
     void Start()
-    {
+    {        
+        gravityLayer = LayerMask.NameToLayer("GravityChange");
+        groundLayer = LayerMask.NameToLayer("Ground");
+        magnetLayer = LayerMask.NameToLayer("GravityMagnet");
         EventSystem.Current.RegisterListener<GravityGunEvent>(OnPlayerShoots, ref _playerShootsGuid);
     }
 
@@ -21,9 +29,31 @@ public class PlayerShotEffectController : MonoBehaviour
             Quaternion.Euler(0, 0, 0));
 
         laser.transform.LookAt(gravityGunEvent.Point);
-        laser.GetComponent<VisualEffect>().SetFloat("LightningLength",
+        var effect = laser.GetComponent<VisualEffect>();
+        effect.SetFloat("LightningLength",
             Vector3.Distance(gravityGunEvent.SourceGameObject.transform.position, gravityGunEvent.Point));
+        
+        if (gravityGunEvent.TargetGameObject.layer == gravityLayer)
+        {
+            effect.SetVector4("FlashColour", colors[1]);
+            effect.SetFloat("ImpactSize", impactSize * 1.5f);
+        }
+        else if (gravityGunEvent.TargetGameObject.layer == magnetLayer)
+        {
+            effect.SetVector4("FlashColour", colors[2]);
+            effect.SetFloat("ImpactSize", impactSize * 1.5f);
+        }
+        else
+        {
+            effect.SetVector4("FlashColour", colors[0]);
+            effect.SetFloat("ImpactSize", impactSize);
+        }
+        
+        
+        
+        
         laser.GetComponent<VisualEffect>().Play();
+        
 
         StartCoroutine(DestroyAfterTime(laser, 0.5f));
     }
