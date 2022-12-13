43d916f292374c06291fd838c2fcc0898ee74604
munchosaurus <martinkings1@live.se>
2022-12-13 12:09:21 +0100

    Test remove wall

---
+++ Project Gravity/Assets/Scripts/Object/ReplaceWall.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = System.Object;
+
+[ExecuteInEditMode]
+public class ReplaceWall : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    public Vector3 dimensions;
+    public bool isGround;
+    public GameObject[] specialPrefabs;
+    public Vector3[] specialPrefabPositions;
+    public GameObject[] emblems;
+
+    public Vector3[] emblemPositions;
+    //public LayerMask[] specialLayerMasks;
+
+    public List<GameObject> lavas;
+
+    private void Awake()
+    {
+        List<GameObject> gos = new List<GameObject>();
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (go.hideFlags != HideFlags.None)
+                continue;
+            if (PrefabUtility.GetPrefabType(go) == PrefabType.Prefab ||
+                PrefabUtility.GetPrefabType(go) == PrefabType.ModelPrefab)
+                continue;
+
+            if (go.gameObject.name == "BuildingBlock_Hazard" && go.transform.position.z != 0)
+            {
+                if (Application.isEditor)
+                {
+                    DestroyImmediate(go);
+                }
+                    
+            }
+        }
+
+        // foreach (var VARIABLE in gos)
+        // {
+        //     VARIABLE.font = fontToUse;
+        // }
+        Debug.Log(gos.Count + " är antalet hazards");
+    }
+
+    public void ReplaceLavas()
+    {
+    }
+}
