e8e37ea0e9acc01febd4bfbf7cd4864d8d6ce40a
munchosaurus <martinkings1@live.se>
2022-12-14 13:22:06 +0100

    Added if unity editor stuff

--- Project Gravity/Assets/Scripts/Object/RemoveIncorrectWall.cs
+++ Project Gravity/Assets/Scripts/Object/RemoveIncorrectWall.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using UnityEngine;
 using UnityEngine.UI;
 using Object = System.Object;
@@ -9,31 +14,27 @@ public class RemoveIncorrectWall : MonoBehaviour
 {
     [SerializeField] List<String> namesToChange;
 
+#if UNITY_EDITOR
     private void Awake()
     {
-        
-        // foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
-        // {
-        //     if (go.hideFlags != HideFlags.None)
-        //         continue;
-        //     if (PrefabUtility.GetPrefabType(go) == PrefabType.Prefab ||
-        //         PrefabUtility.GetPrefabType(go) == PrefabType.ModelPrefab)
-        //         continue;
-        //     
-        //     
-        //     if (namesToChange.Contains(go.gameObject.name) && go.transform.position.z != 0)
-        //     {
-        //         if (Application.isEditor)
-        //         {
-        //             DestroyImmediate(go);
-        //         }
-        //             
-        //     }
-        //     
-        // }
+        foreach (GameObject go in Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[])
+        {
+            if (go.hideFlags != HideFlags.None)
+                continue;
+            if (PrefabUtility.GetPrefabType(go) == PrefabType.Prefab ||
+                PrefabUtility.GetPrefabType(go) == PrefabType.ModelPrefab)
+                continue;
+
+
+            if (namesToChange.Contains(go.gameObject.name) && go.transform.position.z != 0)
+            {
+                if (Application.isEditor)
+                {
+                    DestroyImmediate(go);
+                }
+            }
+        }
     }
 
-    public void ReplaceLavas()
-    {
-    }
+#endif
 }
