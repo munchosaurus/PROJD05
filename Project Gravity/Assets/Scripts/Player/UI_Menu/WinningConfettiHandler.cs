c0e6b9fb19f9b095616a5b42b61115a7febb6da8
munchosaurus <martinkings1@live.se>
2022-12-20 15:43:04 +0100

    Added functionality for confetti effect in winning screen

---
+++ Project Gravity/Assets/Scripts/Player/UI_Menu/WinningConfettiHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WinningConfettiHandler : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particleSystem;
+    private static Guid _playerSucceedsGuid;
+
+    private void Start()
+    {
+        EventSystem.Current.RegisterListener<WinningEvent>(OnPlayerSucceedsLevel, ref _playerSucceedsGuid);
+    }
+
+    public void OnPlayerSucceedsLevel(WinningEvent winningEvent)
+    {
+        particleSystem.Play();
+    }
+}
